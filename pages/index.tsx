89a471d0c9d22e12ad229a5d5c0ce43ab6c14d6d
Jinatey <ijnaaah@gmail.com>
2024-02-19 14:32:38 +0500

    search & trending

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,45 +1,66 @@
 import React from "react";
 import { data } from "@/data";
+import { CiSearch } from "react-icons/ci";
+import { useState } from "react";
 
 export default function Home() {
+  const [text, setText] = useState("");
+
+  const filterBySearch = data?.filter((c) =>
+    c.title.toLowerCase().includes(text.toLowerCase())
+  );
+
   return (
     <div className=" ml-10 bg to-blue-950 text-blue-200">
       <div className="p-3">
         <div className="p-2">
-          lorem 20``
-          <p>dkfkd</p>
+          <label className="flex items-center" htmlFor="">
+            <CiSearch size={35} className="" />
+
+            <input
+              className=" caret-red-500  outline-none w-full bg-slate-700  border-b  py-4"
+              placeholder="Search for movie or Tv series"
+              type="text"
+              name=""
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+            />
+          </label>
         </div>
         <h1 className=" text-xl font-bold ">Trending</h1>
-        <div className="p-2 gap-7 max-w-[1200px]  grid  grid-cols-[repeat(5,250px)]  overflow-x-scroll">
-          {data.map((item) => {
-            let img = "";
-            if (item.isTrending) {
-              img = item.thumbnail.trending.large;
-            }
-            return (
-              <div >
-                <img  src={img} alt="" />
-              </div>
-            );
-          })}
 
-          {/* <img src={data[0].thumbnail.trending?.small} alt="" /> */}
+        <div className="p-2 gap-7  gridauto  max-w-[750px]  ">
+          {data.map((item) => {
+            if (item.isTrending) {
+              const img = item.thumbnail.trending.large;
+              return (
+                <div className="item">
+                  <img src={img} alt="" />
+                </div>
+              );
+            }
+          })}
         </div>
 
         <div className="">
           <p className=" font-bold  text-xl py-4">Recomended for you </p>
-          <div className="grid gap-9 grid-cols-4 ">
-            {data.map((dataItem) => (
-              <div className=" margin-auto">
-                <div className="  absolute w-[300px] h-[150px] z-10 grid justify-center transition-all 	hover:bg-blue-800 tra items-center">
+          <div className="gridrec">
+            {filterBySearch.map((dataItem) => (
+              <div className="relative margin-auto h-[200px]">
+                <div className="absolute w-[100%]  h-[100%] z-10 grid justify-center transition-all 	hover:bg-blue-800 items-center">
                   <img
                     className=""
                     src="assets/icon-bookmark-empty.svg"
                     alt=""
                   />
                 </div>
+
+                {/*  */}
+                {/*  */}
+                {/*  */}
+
                 <img
-                  className="rounded-md  "
+                  className="rounded-md  block w-[100%]"
                   src={dataItem.thumbnail.regular?.small}
                   alt=""
                 />
